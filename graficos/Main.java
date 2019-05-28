60d3e09da280806def87f657269a949dcb4d1570
vosouza <42612500+vosouza@users.noreply.github.com>
2019-05-28 08:52:40 -0300

    separação das classes em pacotes

---
+++ graficos/Main.java
@@ -0,0 +1,10 @@
+package graficos;
+
+public class Main {
+
+	public static void main(String[] args) {
+		FramePrincipal fp = new FramePrincipal();
+		fp.iniciarJanela();
+	}
+
+}
---
+++ logicaJosephus/Josephus.java
@@ -0,0 +1,5 @@
+package logicaJosephus;
+
+public class Josephus {
+
+}
