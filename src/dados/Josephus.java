74910d9696ee288a26449b8c528e77946cbf4767
Vinicius <cpudvini@gmail.com>
2019-06-02 17:17:32 -0300

    em andamento a logica de josephus

--- src/dados/Josephus.java
+++ src/dados/Josephus.java
@@ -1,5 +1,26 @@
 package dados;
 
 public class Josephus {
-
+	
+	private int totalSoldiers;
+	private int step;
+	private DoubleCircledList soldiers;
+	
+	
+	
+	Josephus(int total,int step){
+		this.totalSoldiers = total;
+		this.step = step;
+		
+		soldiers = new DoubleCircledList();
+		for(int i=1; i <= totalSoldiers; i++) {
+			soldiers.addNode(i);
+		}
+	}
+	
+	public int[] getSoldiersAlive() {
+		return null;
+		
+	}
+	
 }
