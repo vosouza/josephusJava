5ecd20e03db1eb2fcc445d558093b58d59662aeb
vosouza <42612500+vosouza@users.noreply.github.com>
2019-06-07 08:58:03 -0300

    Update Animacao.java

--- src/graphics/Animacao.java
+++ src/graphics/Animacao.java
@@ -1,25 +1,39 @@
-//package graphics;
-//
-//public class Animacao extends Thread {
-//	public int tempoEspera;
-//	
-//	
-//	public Animacao(int tempoEspera){
-//		this.tempoEspera = tempoEspera;
-//	}
-//
-//	// Metodo contendo o algoritmo do Josephus
-//	public void run() {
-//
-//		// TESTE
-//		int qtd = getQtdIndividuos();
-//		for (int i = 0; i < qtd; i += 2) {
-//			individuos[i].setBackground(getCorIndividuoExecutado());	
-//			try {
-//				Thread.sleep(this.tempoEspera);
-//			}catch(Exception e) {
-//
-//			}
-//		}
-//	}
-//}
+package graphics2;
+
+import dados.Josephus;
+
+public class Animacao extends Thread {
+	
+	public int tempoEspera;
+	JPBonecos painel;
+	Josephus regra;
+	
+	public Animacao(int tempoEspera){
+		this.tempoEspera = tempoEspera;
+	}
+
+	public Animacao(Josephus regras, JPBonecos bonecos) {
+		// TODO Auto-generated constructor stub
+		this.regra = regras;
+		this.painel = bonecos;
+		this.tempoEspera = 100;
+		painel.adicionarLabelsNoPainelIndividuos(regra.getQuantidade());
+	}
+
+	// Metodo contendo o algoritmo do Josephus
+	public void run() {
+
+		// TESTE
+		int qtd = regra.getQuantidade();
+		for (int i = 0; i < qtd; i++) {
+			painel.adicionarIndividosComGrifo(qtd,regra.getSoldiersAlive());
+			regra.playRound();
+			
+			try {
+				Thread.sleep(this.tempoEspera);
+			}catch(Exception e) {
+
+			}
+		}
+	}
+}
